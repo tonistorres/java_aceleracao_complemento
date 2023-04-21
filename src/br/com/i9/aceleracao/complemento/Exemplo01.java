3310718005ae49cc4b5e1b4cb1aeb6eed4a3ce7a
Tonis Torres <sisvenda2011@gmail.com>
2023-04-21 20:11:57 -0300

    feat: initial commit exercise java intro

---
+++ src/br/com/i9/aceleracao/complemento/Exemplo01.java
@@ -0,0 +1,22 @@
+package br.com.i9.aceleracao.complemento;
+
+import java.util.Locale;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+		double x=10.35784;
+		String firstName="Tonis";
+		int idade=31;
+		double renda=4000.0;
+		System.out.println(x);
+		System.out.printf("%.2f%n",x);
+		System.out.printf("%.4f%n",x);
+		Locale.setDefault(Locale.US);
+		System.out.println("RESULTADO = "+x+" METROS");
+		System.out.printf("RESULTADO = %.2f metros %n",x);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n",firstName,idade,renda);
+
+	}
+
+}
