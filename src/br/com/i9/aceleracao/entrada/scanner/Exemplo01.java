35a303fd66a196228058fb3a571d9b6430af753d
Tonis Torres <sisvenda2011@gmail.com>
2023-04-22 14:08:45 -0300

    feat: adding github

--- src/br/com/i9/aceleracao/entrada/scanner/Exemplo01.java
+++ src/br/com/i9/aceleracao/entrada/scanner/Exemplo01.java
@@ -4,24 +4,30 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Exemplo01 {
-	/**
-	 * No comando next() quando for digitar por exemeplo PRIMEIRO NOME é apenas um
-	 * nome sem espaço... Caso dê um espaço o programa irá quebrar, pois, para esse
-	 * tipo de procedimento temos nextLine() que irá lê a linha toda
-	 */
 	public static void main(String[] args) {
+		/**
+		 * com essa linha de comando estou associando o meu objeto Scanner ao teclado no
+		 * modo console
+		 */
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Entre com PRIMEIRO NOME:");
+		// digite um texto sem espaços
 		String nome = sc.next();
 		System.out.println("Entre com sua IDADE:");
+		// comando sc utilizado para leitura de numero inteiro
 		int idade = sc.nextInt();
 		System.out.println("Digite seu SALÁRIO EM DOLARES:");
+		// utilizando o método Locale para setar valores em dolar para meu sistema
 		Locale.setDefault(Locale.US);
+		// comando sc utilizado para leitura de numeros ponto flutuante
 		double salario = sc.nextDouble();
 		System.out.println("Digite seu sexo: F (Feminino) M (Masculino) O(Outros)");
+		// o método abaixao captura a primeira letra na posição 0 do valor da string digitada
 		char sexo = sc.next().charAt(0);
 		System.out.printf("Meu nome é %s tenho a idade de  %d%n" + "tenho uma renda de %.2f%n" + "meu sexo é %c", nome,
 				idade, salario, sexo);
+
+		/**Desalocando da mémoria esse recurso Scanner criado logo acima(coisas da linguagem)*/
 		sc.close();
 
 	}
