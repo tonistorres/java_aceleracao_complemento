704bd5342b1a8dc79ba08eee795c4f581cfda197
Tonis Torres <sisvenda2011@gmail.com>
2023-04-30 13:43:32 -0300

    feat: implementing exercises programming logic initials

---
+++ src/br/com/i9/exercises/one/RadiusCircle.java
@@ -0,0 +1,27 @@
+package br.com.i9.exercises.one;
+
+import java.util.Locale;
+//import java.util.Locale;
+import java.util.Scanner;
+
+public class RadiusCircle {
+	/**
+	 * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o
+	 * valor da área deste círculo com quatro casas decimais conforme exemplos.
+	 * Fórmula da área: area = π . raio2 Considere o valor de π = 3.14159
+	 */
+	// Declarando uma CONSTANTE:
+	// https://www.devmedia.com.br/java-variaveis-e-constantes/38311#constantes
+
+	public static void main(String[] args) {
+
+		final float PI = 3.14159F; // Declarando uma CONSTANTE EM JAVA
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o raio de um círculo:");
+		double raio = sc.nextDouble();
+		double area = PI * Math.pow(raio, 2);
+		System.out.printf("Área do Raio é:%.4f", area);
+
+	}
+}
---
+++ src/br/com/i9/exercises/one/ReadNumberInteger.java
@@ -0,0 +1,24 @@
+package br.com.i9.exercises.one;
+
+import java.util.Scanner;
+
+public class ReadNumberInteger {
+
+	public static void main(String[] args) {
+		/**
+		 * Classe scanner passando como argumento a expressão Systen.in - Associa o meu
+		 * objeto Scanner ao teclado no modo console ENUNCIADO: Faça um programa para
+		 * ler dois valores inteiros, e depois mostrar na tela a soma desses números com
+		 * uma mensagem explicativa, conforme exemplos.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o PRIMEIRO numero Inteiro:");
+		int primeiroNumero = sc.nextInt();
+		System.out.println("Digite o SEGUNDO numero Inteiro");
+		int segundoNumero = sc.nextInt();
+		System.out.println(primeiroNumero+segundoNumero);
+
+	}
+
+}
